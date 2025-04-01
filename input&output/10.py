10aa6bd3cdb5a5efb225e9d5f046080434faed8c
niranj1234 <46585465+niranj1234@users.noreply.github.com>
2025-04-01 11:04:54 -0700

    Update 10.py

--- input&output/10.py
+++ input&output/10.py
@@ -1,1 +1,17 @@
+# Write a code to get the input in the given format and print the output in the given format.
 
+
+# Input Description:
+# A single line contains a string.
+
+# Output Description:
+# Print the characters in a string separated by comma.
+
+# Sample Input :
+# guvi
+# Sample Output :
+# g,u,v,i
+
+input_string = input()
+output_string = ','.join(input_string)
+print(output_string)
