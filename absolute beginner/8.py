f3c0981979e53dadc98ffbd9c253462240be539b
niranj1234 <46585465+niranj1234@users.noreply.github.com>
2025-04-18 19:28:30 -0700

    Create 8.py

---
+++ absolute beginner/8.py
@@ -0,0 +1,19 @@
+# The area of an equilateral triangle is ¼(√3a2) where "a" represents a side of the triangle. You are provided with the side "a". Find the area of the equilateral triangle.
+
+
+# Input Description:
+# The side of an equilateral triangle is provided as the input.
+
+# Output Description:
+# Find the area of the equilateral triangle and print the answer up to 2 decimal places after rounding off.
+
+# Sample Input :
+# 20
+# Sample Output :
+# 173.21
+
+
+import math
+a=int(input(""))
+area=((math.sqrt(3))/4)*(a*a)
+print("{:.2f}".format(area))
